63b45c515cc7effb7961b231111e01a632668a66
Christian Rondeau <christian.rondeau@gmail.com>
2016-12-21 00:22:25 -0500

    Handle tty res

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
-console.log("Rogue");
+if(!process.stdout.isTTY)
+	throw "Not a TTY";
 
+console.log("Rogue (res: " + process.stdout.rows + "x" + process.stdout.columns + ")");
+
+process.stdout.on('resize', () => {
+	console.log('screen size has changed!');
+	console.log(`${process.stdout.columns}x${process.stdout.rows}`);
+});
